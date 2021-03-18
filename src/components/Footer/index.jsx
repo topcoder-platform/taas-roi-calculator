69fef5d2644d5c64cc7157e6a2ebfb012da52588
nqv <nqvietit@gmail.com>
2021-03-18 21:17:10 +0700

    (18.) Update footer links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable max-len */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './styles.scss';
 
@@ -7,11 +6,11 @@ const Footer = () => (
   <footer styleName="footer">
     <p styleName="content-center">
       By clicking “schedule a taas call” you agree to our Terms and Privacy Policy. We are never going to sell your data or send you spam. Your information is being used for communication purposes only. Wage and salalry data:
-      <a href="#">glassdoor.com</a>
+      <a href="https://www.glassdoor.com/">glassdoor.com</a>
       . Bonus, equity, other benefits & employee overhead data: MIT study
-      <a href="#">How Much Does An Employee Cost?</a>
+      <a href="https://web.mit.edu/e-club/hadzima/pdf/how-much-does-an-employee-cost.pdf">How Much Does An Employee Cost?</a>
       . Recruiting & time to hire data: workable.com
-      <a href="#">Recruiting Costs: Budget and Cost per Hire | Recruiting Metrics FAQ</a>
+      <a href="https://resources.workable.com/tutorial/faq-recruitment-budget-metrics">Recruiting Costs: Budget and Cost per Hire | Recruiting Metrics FAQ</a>
     </p>
   </footer>
 );
