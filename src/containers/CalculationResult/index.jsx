b74b806cc1ce8ed5f668449903d75f9aee1ef2fa
Dian Pamungkas <degraphicart@gmail.com>
2021-04-22 00:19:15 +0700

    Update index.jsx

--- src/containers/CalculationResult/index.jsx
+++ src/containers/CalculationResult/index.jsx
@@ -24,7 +24,286 @@ import IconTelecoms from '../../assets/icons/telecom.svg';
 import IconPublicSector from '../../assets/icons/public-sector.svg';
 import IconTravelHospitality from '../../assets/icons/travel-hospitality.svg';
 
+import jsPDF from "jspdf";
+
 import './styles.scss';
+
+const Prints = () => (
+	<div styleName="page" id="calculationResult">
+      <div styleName="row">
+        <div styleName="col">
+        
+            <div styleName="left-section">
+              <h3 styleName="heading-3 text-gradient" className="upper-case">
+                Hello
+                {' '}
+                <span className="upper-case">{form.firstName}</span>
+                ,
+              </h3>
+              <p styleName="text margin-top-extra">
+                Based on the information you’ve provided us, plus our market research,
+                here is your customized cost comparison for hiring
+                {' '}
+                {utils.format2DigitsNumber(result.numberOfEmployee)}
+                {' '}
+                <span styleName="text-capitalize">
+                  {result.numberOfEmployee > 1 ? utils.formatPluralNoun(form.talentType) : form.talentType}
+                </span>
+                {' '}
+                to
+                {' '}
+                <span styleName="text-capitalize">{form.company}</span>
+                .
+              </p>
+              <p styleName="text">
+                Hiring
+                {' '}
+                {utils.formatPluralNoun(form.talentType)}
+                {' '}
+                on Topcoder’s TaaS platform saves you time
+                and money. Our solution is charged at a flat rate of
+                {' '}
+                <span>{utils.formatMoneyValueI(result.topcoderWeeklyCost)}</span>
+                {' '}
+                per week and
+                freelancers are often able to start within days, not weeks. This allows you
+                to start producing results and raising team productivity quicker.
+              </p>
+              <br/><br/><br/>  
+              
+            </div>
+      
+        </div>
+        
+      </div>
+
+      <div styleName="row">
+      <div styleName="col">
+          <div styleName="card-container">
+          	<div styleName="row">
+			 <div styleName="col">          
+            {/* YOUR RESULT S */}
+            <Card>
+              <Card.Header>
+                <h6 styleName="heading-6" className="text-violet">YOUR RESULT</h6>
+              </Card.Header>
+              <Card.Body>
+                <div styleName="box-container">
+                  <div styleName="box">
+                    <span styleName="value heading-3">{utils.format2DigitsNumber(result.numberOfEmployee)}</span>
+                    <span styleName="description">{form.talentType}</span>
+                  </div>
+                  <div styleName="box padding-extra">
+                    <span styleName="value heading-3">{result.costOfLiving || '\u00a0'}</span>
+                    <span styleName="description">COST OF LIVING</span>
+                  </div>
+                  <div styleName="box">
+                    <span styleName="value heading-3">{industryIcon}</span>
+                    <span styleName="description">{industry}</span>
+                  </div>
+                  <div styleName="box">
+                    <span styleName="value heading-3">{utils.formatMoneyValueK(result.averageSalary)}</span>
+                    <span styleName="description">AVERAGE SALARY</span>
+                  </div>
+                  <div styleName="box">
+                    <span styleName="value heading-3">{utils.formatMoneyValueK(result.bonusEquityBenefits)}</span>
+                    <span styleName="description">{!isMobileOrTablet ? 'BONUS, EQUITY, BENEFITS' : 'BONUSES & BENEFITS'}</span>
+                  </div>
+                  <div styleName="box">
+                    <span styleName="value heading-3">{utils.formatMoneyValueK(result.recruiment + result.overhead)}</span>
+                    <span styleName="description">RECRUITMENT &amp; OVERHEAD</span>
+                  </div>
+                </div>
+                <div styleName="total">
+                  <span styleName="annually">
+                    TOTAL ANNUALLY:
+                    {' '}
+                    <span styleName="value">{utils.formatMoneyValueI(result.totalAnnualCost)}</span>
+                  </span>
+                  <span styleName="weekly">
+                    WEEKLY COST:
+                    {' '}
+                    <span styleName="value">{utils.formatMoneyValueI(result.totalWeeklyCost)}</span>
+                  </span>
+                </div>
+              </Card.Body>
+              <Card.Footer>
+                <div styleName="save">
+                  <span>
+                    TOPCODER WEEKLY COST:
+                    {' '}
+                    {utils.formatMoneyValueI(result.topcoderWeeklyCost)}
+                    {' '}
+                    <small>(+TAX)</small>
+                  </span>
+                  <span>
+                    YOU SAVE:
+                    {' '}
+                    {utils.formatMoneyValueI(result.youSave)}
+                    / WEEK
+                  </span>
+                </div>
+              </Card.Footer>
+            </Card>
+            </div>
+			 <div styleName="col">    
+            {/* VISUAL BREAKDOWN */}
+            <Card>
+              <Card.Header>
+                <h6 styleName="heading-6" className="text-darkCyan">VISUAL BREAKDOWN</h6>
+              </Card.Header>
+              <Card.Body>
+                <div styleName="donut-chart-container">
+                  <div styleName="donut-chart-box">
+                    <DonutChart data={{
+                      annualSalary: result.averageSalary,
+                      bonus: result.bonusEquityBenefits,
+                      recruiment: result.recruiment,
+                      overhead: result.overhead,
+                    }}
+                    />
+                  </div>
+                  <div styleName="donut-legend-box">
+                    <h4 styleName="heading-4">
+                      Total Annual Cost:
+                      {utils.formatMoneyValueI(result.totalAnnualCost)}
+                    </h4>
+                    <div>
+                      <span styleName="label">
+                        <i styleName="dot dot-1" />
+                        {' '}
+                        Annual Salary
+                      </span>
+                      <span styleName="label">
+                        <i styleName="dot dot-2" />
+                        {' '}
+                        Bonus, Equity &amp; Benefits
+                      </span>
+                      <span styleName="label">
+                        <i styleName="dot dot-3" />
+                        {' '}
+                        Recruiment Costs
+                      </span>
+                      <span styleName="label">
+                        <i styleName="dot dot-4" />
+                        {' '}
+                        Employee Overheads
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+            </div>
+             </div>
+             <div styleName="row">
+			 <div styleName="col">    
+            {/* FREELANCER VS. TRADITIONAL HIRING */}
+            <Card>
+              <Card.Header>
+                <h6 styleName="heading-6" className="text-green">FREELANCER VS. TRADITIONAL HIRING</h6>
+              </Card.Header>
+              <Card.Body>
+                <p styleName="text margin-top">
+                  By the time your traditionally hired FTE goes through recruitment, interviews,
+                  negotiations and agreeing on start dates; the talent at Topcoder has already been
+                  working for months at 100% productivity. We can go from the initial request to top-tier
+                  verified talent working for you, fast.
+                </p>
+                <div styleName="line-chart-container">
+                  <div styleName="line-chart-box">
+                    <LineChart height={isMobileOrTablet ? 230 : undefined} />
+                  </div>
+                  <div styleName="line-legend-box">
+                    <span styleName="label">
+                      <i styleName="dot dot-1" />
+                      {' '}
+                      Topcoder TaaS Process
+                    </span>
+                    <span styleName="label">
+                      <i styleName="dot dot-2" />
+                      {' '}
+                      Traditional Hiring Process
+                    </span>
+                  </div>
+                  <div styleName="message">
+                    <h6 styleName="heading-6" className="text-green">AVG. PRODUCTIVITY GAIN:</h6>
+                    <p>280 Hours In The First 3 Months</p>
+                  </div>
+                  <p styleName="footer">
+                    Calculation Based on Average Time to Hire of 30 Days (Traditional FTE Labor) and 40-hours per week usage of Topcoder
+                    TaaS Freelancer(s)
+                  </p>
+                </div>
+              </Card.Body>
+            </Card>
+            </div>
+			 <div styleName="col">    
+            {/* THE TALENT */}
+            <Card>
+              <Card.Header>
+                <h6 styleName="heading-6" className="text-red">THE TALENT</h6>
+              </Card.Header>
+              <Card.Body>
+                <p styleName="text margin-top">
+                  Joe, the talent on our platform are virtual collaboration professionals. They are
+                  used to sliding seamlessly into new teams and exciting projects and are ready to
+                  get to work right away. Here is a snapshot of the talent on our platform.
+                </p>
+                <div styleName="talents">
+                  <SlideShow>
+                    {
+                      talents.map((talent, index) => (
+                        <div
+                          styleName={`talent ${index === 0 ? ' first' : ''}`}
+                          key={talent.member.handle}
+                        >
+                          <CardMember
+                            member={talent.member}
+                            tags={talent.tags}
+                            wins={talent.wins}
+                            skills={talent.skills}
+                            experience={talent.experience}
+                          />
+                        </div>
+                      ))
+                    }
+                  </SlideShow>
+                </div>
+              </Card.Body>
+            </Card>
+             <br/>
+            <p styleName="text bolder">
+                If you’re ready to see how Topcoder can help turbocharge your team’s
+                results and output, schedule a demo with us today.
+              </p>
+              { !utils.platform.isMobileOS() && (
+                <div styleName="buttons">
+                  <PrimaryButton to={process.env.CALENDLY_URL} size={isMobileOrTablet ? 'sm' : ''}>SCHEDULE A TAAS DEMO</PrimaryButton>
+                   <SecondaryButton onClick={() => { navigateTo(`/result?${utils.url.createUrlQueryString()}`); }} >DOWNLOAD RESULTS</SecondaryButton>
+                </div>
+               
+
+              )}
+            </div> 
+
+            </div>
+           
+          </div>
+        </div>
+        </div>
+
+    </div>
+);
+
+const print = () => {
+  const string = renderToString(<Prints />);
+  const pdf = new jsPDF();
+  pdf.fromHTML(string);
+  pdf.save('pdf')
+}
+
+
 
 const CalculationResult = ({
   form,
@@ -112,7 +391,7 @@ const CalculationResult = ({
               { !utils.platform.isMobileOS() && (
                 <div styleName="buttons">
                   <PrimaryButton to={process.env.CALENDLY_URL} size={isMobileOrTablet ? 'sm' : ''}>SCHEDULE A TAAS DEMO</PrimaryButton>
-                  <SecondaryButton>DOWNLOAD RESULTS</SecondaryButton>
+                 <SecondaryButton onClick={print} >DOWNLOAD RESULT S</SecondaryButton>
                 </div>
               )}
             </div>
@@ -307,9 +586,7 @@ const CalculationResult = ({
             </Card>
 
             {/* RECALCULATE */}
-            <div styleName="recalculate">
-          
-            </div>
+           
           </div>
         </div>
       </div>
