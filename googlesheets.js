bb00041da876798cc72e48844cfd7a25d9f51041
Dedy Wahyudi <d3dyw4hyud1@gmail.com>
2021-04-26 17:06:11 +0700

    put back validator

--- googlesheets.js
+++ googlesheets.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { google } = require('googleapis');
 const config = require('config');
 const joi = require('joi');
+const validator = require('express-joi-validation').createValidator({});
 const sheetApiKey = process.env.GCP_KEY_FILE;
 
 
