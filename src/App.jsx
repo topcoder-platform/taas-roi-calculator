ba1c84d583bee8fc50bdc9291ec536f2d613dcb6
Muneem <r_raja01@hotmail.com>
2021-05-08 16:11:12 +0400

    refactor: new pdf

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
         <Header />
         <Routes />
         <Footer />
-        {process.env.NODE_ENV === 'development' && <DevTools />}
+        {/*{process.env.NODE_ENV === 'development' && <DevTools />}*/}
       </BrowserRouter>
     </Provider>
   );
