7409cae8c9d2c7fcd172eca5b85e7cd8f248672f
Muneem <r_raja01@hotmail.com>
2021-05-22 00:04:21 +0400

    added favicons in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -115,7 +115,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
       inject: 'body',
-      favicon: "./favicon.ico"
+      favicon: './favicon.ico'
     }),
     new MiniCssExtractPlugin({
       chunkFilename: `[name].css`,
